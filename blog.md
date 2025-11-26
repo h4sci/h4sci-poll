0976daa63ed1d5d5237182a6f5fe2f1201e1268f
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-11-26 14:09:41 +0100

    doing h4sci-poll based on matt's initial blog post

---
+++ blog.md
@@ -0,0 +1,8 @@
+**Notes:**
+
+- if you want to run the app locally, before containerising, you should 
+
+`library(shiny)` then cd to dir where you ui.R and server.R is located, then `runApp()` there.
+
+but, cannot submit yet, will get an error. why? because db_container (i.e. our DB) does not exist yet! use this just for inspection of how front end looks like.
+
---
+++ ui.R
@@ -0,0 +1,19 @@
+# library(shinythemes)
+
+fluidPage(
+  theme = shinytheme("superhero"),
+  title = "Hacking for Science - Demo Survey",
+  fluidRow(
+    column(
+      width = 6,
+      div(
+        class = "jumbotron",
+        h1("Hacking for Science"),
+        p("Some Introductory paragraph motivating our survey.")
+      )
+    )
+  ),
+  uiOutput("basic_questions"),
+  uiOutput("submit"),
+  uiOutput("thanks")
+)
